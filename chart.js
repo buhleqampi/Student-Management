fa22f8796f01be8230d7ba2a3e0b1a5eebbb276f
buhleqampi <buhle3857@gmail.com>
2024-09-12 09:31:02 +0200

    chart is working

--- chart.js
+++ chart.js
@@ -17,7 +17,7 @@ async function getStudentsData() {
     studentsData = data;
     console.log("I am Sis Pedro")
     drawLineGraph();
-    // drawPieChart();
+    drawPieChart();
   } catch (error) {
     console.error("Error fetching students data:", error.message);
   }
@@ -62,6 +62,40 @@ function drawLineGraph() {
       }
     });
   }
+
+  function drawPieChart() {
+    const ctx = document.getElementById('yearDistributionPie').getContext('2d');
+    const yearCounts = {};
+  
+    studentsData.forEach(student => {
+      yearCounts[student.yearOfStudy] = (yearCounts[student.yearOfStudy] || 0) + 1;
+    });
+  
+    const yearLabels = Object.keys(yearCounts);
+    const yearData = Object.values(yearCounts);
+  
+    new Chart(ctx, {
+      type: 'pie',
+      data: {
+        labels: yearLabels,
+        datasets: [{
+          data: yearData,
+          backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)'],
+          borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)'],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          }
+        }
+      }
+    });
+  }
+  
   
 
   document.addEventListener('DOMContentLoaded', getStudentsData);
